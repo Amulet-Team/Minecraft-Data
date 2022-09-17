46d782e66ccdba680936fb22c4b9d0df8d5e88b7
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-09-17 10:46:37 +0100

    Added code to get the block data version

---
+++ platforms/bedrock/generators/BedrockData/BedrockData/block_version.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+
+#include <fstream>
+#include <string>
+
+#include <MC/HashedString.hpp>
+#include "get_file.hpp"
+
+
+THook(void, "?prepareBlocks@BlockTypeRegistry@@SAXI@Z", unsigned int version) {
+    *getFile("generated/block/version.txt") << version << std::endl;
+    original(version);
+}
