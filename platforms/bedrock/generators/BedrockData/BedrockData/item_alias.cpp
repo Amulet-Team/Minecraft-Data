36550622d0f1f520026290bc9b527aa512f5b549
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-09-17 16:34:33 +0100

    Extended item generators

--- platforms/bedrock/generators/BedrockData/BedrockData/item_alias.cpp
+++ platforms/bedrock/generators/BedrockData/BedrockData/item_alias.cpp
@@ -10,16 +10,25 @@
 
 
 THook(void, "?registerAlias@ItemRegistry@@SAXAEBVHashedString@@0AEBVBaseGameVersion@@@Z", const HashedString& alias, const HashedString& name, const BaseGameVersion& game_version) {
-    *getFile("generated/item/alias.txt") << alias.getString() << "|" << name.getString() << std::endl;
+    *getFile("generated/item/alias.txt") << alias.getString() << "|" << name.getString() << "|" << game_version.asSemVersion().asString() << std::endl;
     original(alias, name, game_version);
 }
 
 
 THook(Item&, "?registerComplexAlias@ItemRegistry@@SAAEAVItem@@AEBVHashedString@@V?$function@$$A6A?AVHashedString@@F@Z@std@@@Z", const HashedString& alias, std::function<HashedString(short)> func) {
-	std::function<HashedString(short)> func_wrap = [alias, func](signed short data) {
-		HashedString str = func(data);
-		*getFile("generated/item/complex_alias.txt") << alias.getString() << "|" << data << "|" << str.getString() << std::endl;
-		return str;
-	};
-	return original(alias, func_wrap);
+	for (auto i = 0; i < 256; i++) {
+		HashedString name = func(i);
+		*getFile("generated/item/complex_alias.txt") << alias.getString() << "|" << i << "|" << name.getString() << std::endl;
+	}
+	return original(alias, func);
+}
+
+THook(void, "?registerLegacyID@ItemRegistry@@SAXAEBVHashedString@@F@Z", const HashedString& name, short id) {
+	*getFile("generated/item/legacy_id.txt") << id << "|" << name.getString() << std::endl;
+	original(name, id);
+}
+
+THook(void, "?registerLegacyMapping@ItemRegistry@@SAXAEBVHashedString@@0AEBVBaseGameVersion@@@Z", const HashedString& alias, const HashedString& name, const BaseGameVersion& game_version) {
+	*getFile("generated/item/legacy_mapping.txt") << alias.getString() << "|" << name.getString() << "|" << game_version.asSemVersion().asString() << std::endl;
+	original(alias, name, game_version);
 }
