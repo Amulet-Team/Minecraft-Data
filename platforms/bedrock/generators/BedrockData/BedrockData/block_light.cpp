e3f2a4f7091260627e0a829bdc08dff2ef5e2974
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-09-25 12:04:37 +0100

    Reverted lighting changes
    I don't know why it did not give the correct values

--- platforms/bedrock/generators/BedrockData/BedrockData/block_light.cpp
+++ platforms/bedrock/generators/BedrockData/BedrockData/block_light.cpp
@@ -1,11 +1,19 @@
 #pragma once
 #include "pch.h"
 
+#include <MC/Block.hpp>
+#include <MC/Brightness.hpp>
+
 #include "minecraft.hpp"
 #include "get_file.hpp"
 
 
-void block_light(const Block_* block) {
+void block_light(const Block* block) {
+    Brightness brightness = block->getLight();
+    int light = *(unsigned char*)&brightness;
+    *getFile("generated/block/data/light.txt") << light << std::endl;
+
+    /*
     typedef Brightness_(*getLightT)(const Block_*);
     auto getLight = (getLightT)dlsym("?getLight@Block@@QEBA?AUBrightness@@XZ");
     if (getLight != NULL) {
@@ -16,4 +24,5 @@ void block_light(const Block_* block) {
     else {
         *getFile("generated/err.txt") << "?getLight@Block@@QEBA?AUBrightness@@XZ" << std::endl;
     }
+    */
 }
