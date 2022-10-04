7fbe4e7173a9b7b09cfe7fbff3c0348c0c205113
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-10-04 11:59:08 +0100

    Expanded block data plugin
    This plugin now exports the block state and the block entity data to a file.
    These are two consecutive binary NBT fields.
    If a value is null it is replaced with a TAG_Byte value of 0

--- platforms/bedrock/generators/BlockDataPlugin/BlockDataPlugin/dllmain.cpp
+++ platforms/bedrock/generators/BlockDataPlugin/BlockDataPlugin/dllmain.cpp
@@ -3,8 +3,8 @@
 #include "pch.h"
 #pragma comment(lib, "bedrock_server_api.lib")
 #pragma comment(lib, "bedrock_server_var.lib")
-#pragma comment(lib, "../LiteLoaderSDK/Lib/SymDBHelper.lib")
-#pragma comment(lib, "../../LLPreLoader/LLPreLoader.lib")
+#pragma comment(lib, "SymDBHelper.lib")
+#pragma comment(lib, "LLPreLoader.lib")
 
 #include <fstream>
 #include <Windows.h>
@@ -12,7 +12,7 @@
 #include <MC/NbtIo.hpp>
 #include <MC/CompoundTag.hpp>
 #include <MC/BlockActor.hpp>
-#include <MC/BlockPos.hpp>
+#include <MC/Block.hpp>
 #include <MC/BlockSource.hpp>
 #include <MC/Dimension.hpp>
 #include <HookAPI.h>
@@ -38,8 +38,10 @@ std::string write_compound(const CompoundTag* tag) {
     return result;
 }
 
-
-std::string lastValue = "";
+using namespace std::string_literals;
+std::string lastBlockStr = "";
+std::string lastBlockEntityStr = "";
+std::string empty = "\x01\x00\x00\x00"s;
 
 
 
@@ -47,19 +49,44 @@ TInstanceHook(void, "?tick@Level@@UEAAXXZ", Level) {
     Dimension* dim = this->getDimension(0);
     if (dim != NULL) {
         BlockSource* blockSource = &dim->getBlockSourceFromMainChunkSource();
+        bool changed = false;
+        const Block* block = &blockSource->getBlock(100, 100, 100);
+        if (block == NULL) {
+            if (lastBlockStr != empty) {
+                lastBlockStr = empty;
+                changed = true;
+            };
+        }
+        else {
+            const CompoundTag* tag = &block->getSerializationId();
+            std::string blockStr = write_compound(tag);
+            if (lastBlockStr != blockStr) {
+                lastBlockStr = blockStr;
+                changed = true;
+            }
+        }
+        
         BlockActor* blockActor = blockSource->getBlockEntity(100, 100, 100);
         if (blockActor == NULL) {
-            lastValue = "";
+            if (lastBlockEntityStr != empty) {
+                lastBlockEntityStr = empty;
+                changed = true;
+            };
         }
         else {
             auto compound = CompoundTag::fromBlockActor(blockActor);
-            std::string out = write_compound(compound.get());
-            if (lastValue != out) {
-                lastValue = out;
-                std::ofstream file ("block_entity.nbt", std::ios::binary);
-                file << out;
-                std::cout << "nbt changed" << std::endl;
+            std::string blockEntityStr = write_compound(compound.get());
+            if (lastBlockEntityStr != blockEntityStr) {
+                lastBlockEntityStr = blockEntityStr;
+                changed = true;
             }
+        }
+
+        if (changed) {
+            std::ofstream file("block_data.nbt", std::ios::binary);
+            file << lastBlockStr;
+            file << lastBlockEntityStr;
+            std::cout << "block changed" << std::endl;
         }
     }
     original(this);
