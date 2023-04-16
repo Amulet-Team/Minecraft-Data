cd1a081fb389577e04067589b99d1e9e3cdbe0fc
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-04-16 09:52:24 +0100

    Save block data

--- platforms/bedrock/generators/BedrockData/BedrockData/generators/block.cpp
+++ platforms/bedrock/generators/BedrockData/BedrockData/generators/block.cpp
@@ -2,17 +2,35 @@
 #include "MC/Minecraft.hpp"
 #include "MC/Level.hpp"
 #include "MC/BlockPalette.hpp"
+#include "MC/nbt/NbtIo.hpp"
 #include "logging.hpp"
+#include <string>
 
 
 using namespace MinecraftAPI;
 
+
 namespace Data {
 	void block() {
+		debug("block generator");
 		Minecraft* minecraft = Minecraft::get();
-		Level* level = minecraft->getLevel();
-		if (level != nullptr) {
-			BlockPalette* block_palette = level->getBlockPalette();
+		debug("got minecraft ptr");
+		if (Level* level = minecraft->getLevel()) {
+			debug("got level ptr");
+			if (BlockPalette* block_palette = level->getBlockPalette()) {
+				debug("got block palette ptr");
+				unsigned int palette_len = block_palette->len();
+				debug(std::to_string(palette_len));
+				for (unsigned int i = 0; i < palette_len; i++) {
+					const Block* block = block_palette->getBlock(i);
+					//logToFile("generated/block/data/friction.txt", block->getFriction());
+					const CompoundTag* tag = block->getSerializationId();
+					std::string tag_data = NbtIo::serialise(tag);
+					logToBinaryFile("generated/block/states.bin", tag_data);
+					logToFile("generated/block/is_container.txt", block->isContainerBlock());
+					logToFile("generated/block/block_entity.txt", block->getBlockEntityType());
+				}
+			}
 		}
 	}
 }
