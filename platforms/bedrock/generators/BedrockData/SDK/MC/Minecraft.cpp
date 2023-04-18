c42cb7e0d5787ef29761bbb95f7ea4c05ed88a9f
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-04-18 16:15:21 +0100

    Wait until the server has full set up
    There might be a better way to achieve this but I couldn't find it.
    This waits for "Server started." to be logged before generating the data. All the data should be valid at this point.

--- platforms/bedrock/generators/BedrockData/SDK/MC/Minecraft.cpp
+++ platforms/bedrock/generators/BedrockData/SDK/MC/Minecraft.cpp
@@ -2,6 +2,7 @@
 #include "HookAPI.h"
 #include <mutex>
 #include <string>
+#include <bitset>
 
 using namespace MinecraftAPI;
 
@@ -9,6 +10,26 @@ using namespace MinecraftAPI;
 Minecraft** Minecraft::Ptr = (Minecraft**)dlsym("?mGame@ServerCommand@@1PEAVMinecraft@@EA");
 // Added 1.11.4.2
 Minecraft::_getLevelT Minecraft::_getLevel = (Minecraft::_getLevelT)dlsym("?getLevel@Minecraft@@QEBAPEAVLevel@@XZ");
+
+
+bool isReady = false;
+
+
+THook(void, "?log@BedrockLog@@YAXIIPEBDH0ZZ", unsigned int a,unsigned int b,char const * c,int d,char const * e, va_list args) {
+	debug("old log");
+	if (std::string(e) == "Server started.") {
+		isReady = true;
+	}
+	original(a, b, c, d, e, args);
+}
+
+THook(void, "?log@BedrockLog@@YAXW4LogCategory@1@V?$bitset@$02@std@@W4LogRule@1@W4LogAreaID@@IPEBDH4ZZ", enum LogCategory a, class std::bitset<3> b, enum LogRule c, enum LogAreaID d, unsigned int e, char const* f, int g, char const* h, va_list args) {
+	if (std::string(h) == "Server started.") {
+		isReady = true;
+	}
+	original(a, b, c, d, e, f, g, h, args);
+}
+
 
 int state = 0;  // 0=unchecked, -1=error, 1=success
 std::string err;
@@ -32,7 +53,7 @@ Minecraft* Minecraft::get() {
 		}
 		else {
 			for (int i = 0; i < 30 * 4; i++) {
-				if (*Minecraft::Ptr == nullptr) {
+				if (!isReady or *Minecraft::Ptr == nullptr) {
 					// This is not set until the server is set up so wait until it is ready
 					std::this_thread::sleep_for(std::chrono::milliseconds(250));
 				}
