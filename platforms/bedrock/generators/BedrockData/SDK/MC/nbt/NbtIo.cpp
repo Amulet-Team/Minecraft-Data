5b602f3e037feaecea296bdb5a6d140658e68bfe
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-04-26 11:07:31 +0100

    Fixed nbt encoding
    The function descriptor changed slightly

--- platforms/bedrock/generators/BedrockData/SDK/MC/nbt/NbtIo.cpp
+++ platforms/bedrock/generators/BedrockData/SDK/MC/nbt/NbtIo.cpp
@@ -5,3 +5,5 @@ using namespace MinecraftAPI;
 
 // Added 1.9.0.15
 NbtIo::_writeT NbtIo::_write = (NbtIo::_writeT)dlsym("?write@NbtIo@@SAXPEBVCompoundTag@@AEAVIDataOutput@@@Z");
+// Changed 1.20.71.01
+NbtIo::_writeT NbtIo::_write2 = (NbtIo::_writeT)dlsym("?write@NbtIo@@YAXPEBVCompoundTag@@AEAVIDataOutput@@@Z");
--- platforms/bedrock/generators/BedrockData/SDK/MC/nbt/NbtIo.hpp
+++ platforms/bedrock/generators/BedrockData/SDK/MC/nbt/NbtIo.hpp
@@ -7,11 +7,16 @@ namespace MinecraftAPI {
 	class NbtIo {
 		typedef void (*_writeT)(CompoundTag const*, IDataOutput&);
 		static _writeT _write;
+		static _writeT _write2;
 	public:
 		static std::string serialise(const CompoundTag* tag) {
 			std::string result = "";
 			IDataOutput iDataOutput(&result);
-			_write(tag, iDataOutput);
+			if (_write){
+				_write(tag, iDataOutput);
+			} else if (_write2){
+				_write2(tag, iDataOutput);
+			}
 			return result;
 		}
 	};
